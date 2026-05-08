a2d6fcc00e29cd1c47c4bd8390e4ff65844097f2
DevJeffreyG <gabrielpaez.r@hotmail.com>
2026-05-08 17:33:13 -0500

    changelog admin-panel

--- src/app/shared/components/latest-changes/latest-changes.component.ts
+++ src/app/shared/components/latest-changes/latest-changes.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Changelog } from '../../interfaces/changelog';
+import { ChangelogService } from '../../services/changelog.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-latest-changes',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './latest-changes.component.html',
   styleUrl: './latest-changes.component.css',
 })
-export class LatestChangesComponent {
+export class LatestChangesComponent implements OnInit, OnDestroy {
+  changelogs: Changelog[] = [];
+  private subscription = new Subscription();
 
+  constructor(private changelogService: ChangelogService) {}
+
+  ngOnInit() {
+    this.subscription.add(
+      this.changelogService.changelogs$.subscribe((changelogs) => {
+        this.changelogs = changelogs;
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
