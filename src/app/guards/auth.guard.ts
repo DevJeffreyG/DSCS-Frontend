7dd74b08c3898e35f8a15e9da041e45c157f89ed
DevJeffreyG <gabrielpaez.r@hotmail.com>
2026-05-10 14:17:53 -0500

    fix: permisos y svgs

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,13 +1,20 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
+import { UserRole } from '../shared/enums/user-role';
+import { LoggeduserService } from '../shared/services/loggeduser.service';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = (_, state) => {
   const router = inject(Router);
 
   const isLogged = localStorage.getItem('auth') === 'true';
 
   if (!isLogged) {
     router.navigate(['/signin']);
+    return false;
+  }
+
+  if (LoggeduserService.getUser()?.rol === UserRole.Operator && state.url === '/config') {
+    router.navigate(['/dashboard']);
     return false;
   }
 
